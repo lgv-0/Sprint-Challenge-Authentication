808756cbfd12a87c3f941d0fa3399c4383eefcdd
Logan Van Hook <loganvan@att.net>
2020-03-27 11:16:09 -0400

    Database Helper

---
+++ database/dbHelper.js
@@ -0,0 +1,23 @@
+let DB = require("knex")(require("./dbConfig"));
+
+function GetUsers()
+{
+    return DB.select("id", "username").from("users");
+}
+
+function GetUser(username)
+{
+    return DB.select("*").from("users").where("username", username);
+}
+
+function InsertUser(username, password)
+{
+    return DB.insert({username, password}).into("users");
+}
+
+module.exports =
+    {
+        GetUsers,
+        GetUser,
+        InsertUser
+    };
