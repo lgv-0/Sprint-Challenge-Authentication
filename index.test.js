22016b2b930d7a0da3d3d1e2a3784a9a4c15d1c4
Logan Van Hook <loganvan@att.net>
2020-03-27 12:00:26 -0400

    Test joke directory

--- index.test.js
+++ index.test.js
@@ -1,3 +1,20 @@
 const request = require('supertest');
 const server = require('./index.js');
 
+describe("Jokes Directory", ()=>
+{
+    it("Denies bad tokens", async ()=>
+    {
+        let response = await request(server).get("/api/jokes");
+        expect(response.status).toEqual(401);
+        response = await request(server).get("/api/jokes").set({"Authorization":"222"});
+        expect(response.status).toEqual(401);
+    });
+
+    it("Returns array on success", async ()=>
+    {
+        let response = await request(server).get("/api/jokes").set({"Authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODUzMjQ0NjYsImV4cCI6MTkwMDkwMDQ2Nn0.cksBQN9uX7CJZtuiXSx8_YUDVjGtqwbeW-vPXCLobqM"});
+        expect(response.status).toEqual(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
+});
